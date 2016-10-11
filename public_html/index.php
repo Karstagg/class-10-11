53f1b2b8c642690bc7c16e9849d9b4df944113cf
Matthew Fisher <m.r.f.48215@gmail.com>
2016-10-11 15:13:28 -0600

    added more css for header text

--- public_html/index.php
+++ public_html/index.php
@@ -17,8 +17,12 @@
 	<body>
 		<!-- the header -->
 		<header>
-			<h1>Hello World</h1>
-			<h4>Welcome to my halfassed website</h4>
+			<div class="header-main-text">
+				<h1>Hello World</h1>
+			</div>
+			<div class="header-sub-text">
+				<h4>Welcome to my halfassed website</h4>
+			</div>
 		</header>
 		<!--main content-->
 		<main>
@@ -28,8 +32,34 @@
 				<p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.</p>
 				<img src="img/party.jpg" alt="You party like shit">
 			</section>
-			<section></section>
-			<section></section>
+			<section>
+				<h2>Stuff I like</h2>
+					<ul>
+						<li>food</li>
+						<li>water</li>
+						<li>etc</li>
+					</ul>
+			</section>
+			<section>
+				<table>
+					<tr>
+						<th>Column One</th>
+						<th>Column Two</th>
+					</tr>
+					<tr>
+						<td>Cat</td>
+						<td>Dog</td>
+					</tr>
+					<tr>
+						<td>Car</td>
+						<td>Bike</td>
+					</tr>
+					<tr>
+						<td>Starwars</td>
+						<td>Wine</td>
+					</tr>
+				</table>
+			</section>
 		</main>
 	</body>
 </html>
