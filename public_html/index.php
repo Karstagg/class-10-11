9f8a80462169f2f5aecf6064996b3801c9f7cb21
Matthew Fisher <m.r.f.48215@gmail.com>
2016-10-11 13:31:30 -0600

    updated index.php and main.css and adding img party.jpg

--- public_html/index.php
+++ public_html/index.php
@@ -15,7 +15,24 @@
 		<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/8bit-wonder" type="text/css"/>
 	</head>
 	<body>
-			<h1>hi!</h1>
+		<!-- the header -->
+		<header>
+			<h1>Hello World</h1>
+			<h4>Welcome to my halfassed website</h4>
+		</header>
+		<!--main content-->
+		<main>
+			<!--begin section 1-->
+			<section>
+				<!--samuel l ipsum-->
+				<p>Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a
+					transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong
+					to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass.</p>
+				<img src="img/party.jpg" alt="You party like shit">
+			</section>
+			<section></section>
+			<section></section>
+		</main>
 	</body>
 </html>
 
