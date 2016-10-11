dba7217a8cd867f5e9aa4ba37f08f08a45694b8f
Matthew Fisher <m.r.f.48215@gmail.com>
2016-10-11 11:31:15 -0600

    updated index.php and main.css

--- public_html/index.php
+++ public_html/index.php
@@ -5,14 +5,19 @@
  * Date: 10/11/2016
  * Time: 10:18 AM
  */
+
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 	<head>
+		<meta charset="utf-8">
 		<link rel="stylesheet" href="css/main.css" type="text/css" />
+		<link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/8bit-wonder" type="text/css"/>
 	</head>
 	<body>
-		<p>hi!</p>
+		<div class="centerText">
+			<h1>hi!</h1>
+		</div>
 	</body>
 </html>
 
