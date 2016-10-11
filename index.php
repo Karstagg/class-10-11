caf626377c520291cfcb8c69682ef6fac031831e
Matthew Fisher <m.r.f.48215@gmail.com>
2016-10-11 10:20:33 -0600

    added index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matthew
+ * Date: 10/11/2016
+ * Time: 10:18 AM
+ */
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/main.css" type="text/css" />
+	</head>
+	<body>
+
+	</body>
+</html>
+
